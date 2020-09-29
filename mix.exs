f94bb37fa85b1021a3d3308e6e7097cd9074f515
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-29 15:08:06 +0200

    pub

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule UniversalDetector.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "UniversalDetector",
+      description: description(),
+      package: package()
     ]
   end
 
@@ -21,8 +24,19 @@ defmodule UniversalDetector.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "This package allows you to detect encoding of given binary resource"
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/patrykwozinski/universal-detector"}
     ]
   end
 end
