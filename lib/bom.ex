455db00fcdc3faeae355bfd454b302f45c0e142e
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-29 12:03:42 +0200

    Detect utf8

--- lib/bom.ex
+++ lib/bom.ex
@@ -1,21 +1,19 @@
 defmodule UniversalDetector.BOM do
-  import String
-
-  @bom_utf8 "\\xef\\xbb\\xbf"
+  @bom_utf8 <<0xEF, 0xBB, 0xBF>>
   @bom_utf16_le "\\xff\\xfe"
   @bom_utf16_be "\\xfe\\xff"
   @bom_utf32_le "\\xff\\xfe\\x00\\x00"
   @bom_utf32_be "\\x00\\x00\\xfe\\xff"
 
   def is_utf8(string) do
-    string |> starts_with?(@bom_utf8)
+    string |> String.first() == @bom_utf8
   end
 
   def is_utf16(string) do
-    string |> starts_with?(@bom_utf16_le) or string |> starts_with?(@bom_utf16_be)
+    string |> String.starts_with?(@bom_utf16_le) or string |> String.starts_with?(@bom_utf16_be)
   end
 
   def is_utf32(string) do
-    string |> starts_with?(@bom_utf32_le) or string |> starts_with?(@bom_utf32_be)
+    string |> String.starts_with?(@bom_utf32_le) or string |> String.starts_with?(@bom_utf32_be)
   end
 end
