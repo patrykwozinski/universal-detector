a22933d076f50ca5e34dc35bd29ceacaf5a3e8a4
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-29 14:59:58 +0200

    cleanup

--- lib/universal_detector.ex
+++ lib/universal_detector.ex
@@ -1,5 +1,5 @@
 defmodule UniversalDetector do
-  alias UniversalDetector.BOM
+  import UniversalDetector.BOM
 
   @moduledoc """
   Detector helps to define encoding of the given data
@@ -13,9 +13,9 @@ defmodule UniversalDetector do
 
   def feed(byte_str) do
     cond do
-      BOM.is_utf8(byte_str) -> "utf8"
-      BOM.is_utf16(byte_str) -> "utf16"
-      BOM.is_utf32(byte_str) -> "utf32"
+      is_utf8(byte_str) -> "utf8"
+      is_utf16(byte_str) -> "utf16"
+      is_utf32(byte_str) -> "utf32"
       true -> @encoding_not_found
     end
   end
