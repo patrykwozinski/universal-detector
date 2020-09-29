b520a41112e2af5e6fa2124ebad016d3f4e2ba71
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-29 14:16:38 +0200

    comparion

--- lib/bom.ex
+++ lib/bom.ex
@@ -1,19 +1,27 @@
 defmodule UniversalDetector.BOM do
-  @bom_utf8 <<0xEF, 0xBB, 0xBF>>
-  @bom_utf16_le "\\xff\\xfe"
-  @bom_utf16_be "\\xfe\\xff"
-  @bom_utf32_le "\\xff\\xfe\\x00\\x00"
-  @bom_utf32_be "\\x00\\x00\\xfe\\xff"
+  @utf8 <<0xEF, 0xBB, 0xBF>>
+  @utf16_le <<0xFF, 0xFE>>
+  @utf16_be <<0xFE, 0xFF>>
+  @utf32_le <<0xFF, 0xFE, 0x00, 0x00>>
+  @utf32_be <<0x00, 0x00, 0xFE, 0xFF>>
+
+  @x_iso_10646_ucs_4_3412 <<0xFE, 0xFF, 0x00, 0x00>>
 
   def is_utf8(string) do
-    string |> String.first() == @bom_utf8
+    string |> String.first() == @utf8
   end
 
   def is_utf16(string) do
-    string |> String.starts_with?(@bom_utf16_le) or string |> String.starts_with?(@bom_utf16_be)
+    unicode = string |> String.first()
+
+    is_le = unicode !== @utf32_le and unicode == @utf16_le
+    is_be = unicode !== @x_iso_10646_ucs_4_3412 and unicode == @utf16_be
+
+    is_le or is_be
   end
 
   def is_utf32(string) do
-    string |> String.starts_with?(@bom_utf32_le) or string |> String.starts_with?(@bom_utf32_be)
+    unicode = string |> String.first()
+    unicode == @utf32_be
   end
 end
