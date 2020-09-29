be5573862275fd6b17254cd6e122f1cb808d696f
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-29 14:29:41 +0200

    failing utf16 test

--- test/universal_detector_test.exs
+++ test/universal_detector_test.exs
@@ -6,8 +6,18 @@ defmodule UniversalDetectorTest do
   end
 
   test "utf8 detected" do
-    res = File.read!("test/fixtures/utf8.csv")
+    res = load_fixture("utf8.csv")
 
     assert UniversalDetector.feed(res) == "utf8"
+  end
+
+  test "utf16 detected" do
+    res = load_fixture("utf16.csv")
+
+    assert UniversalDetector.feed(res) == "utf16"
+  end
+
+  defp load_fixture(name) do
+    File.read!("test/fixtures/" <> name)
   end
 end
