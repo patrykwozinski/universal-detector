f7d0065404d58ec9589370d5a786d3a1f929652b
Patryk Woziński <patryk.wozinski@gmail.com>
2020-09-29 09:15:12 +0200

    BOM cleanup

--- lib/bom.ex
+++ lib/bom.ex
@@ -1,11 +1,7 @@
 defmodule UniversalDetector.BOM do
-  @BOM_UTF8 b'\xef\xbb\xbf'
-
-  @bom_utf16_le b'\xff\xfe'
-
-  @bom_utf16_be b'\xfe\xff'
-
-  @bom_utf32_le b'\xff\xfe\x00\x00'
-
-  @bom_utf_32_be b'\x00\x00\xfe\xff'
+  @bom_utf8 "\\xef\\xbb\\xbf"
+  @bom_utf16_le "\\xff\\xfe"
+  @bom_utf16_be "\\xfe\\xff"
+  @bom_utf32_le "\\xff\\xfe\\x00\\x00"
+  @bom_utf_32_be "\\x00\\x00\\xfe\\xff"
 end
